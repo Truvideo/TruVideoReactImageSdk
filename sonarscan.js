08770cf57abcec13e2dab465c3dca03ae8c33c70
BhaveshPathe <bhaveshpathe@BhaveshPatheMacAir.local>
2026-01-09 19:06:36 +0530

    build(release): publish SDK 78.1

---
+++ sonarscan.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const scanner = require("sonarqube-scanner");
+
+scanner(
+  {
+    serverUrl: "http://localhost:9000",
+    token: process.env.SONAR_TOKEN,
+    options: {
+      "sonar.projectName": "TruVideoImageSdk",  // Change this
+      "sonar.projectKey": "TruVideoImageSdk",   // Change this
+      "sonar.sources": "src",
+      "sonar.ignore": "src/**/*.test.js,src/**/*.spec.js,src/**/*.test.jsx,src/**/*.spec.jsx,android/**,ios/**,node_modules/**",
+    },
+  },
+  () => process.exit()
+);
+
