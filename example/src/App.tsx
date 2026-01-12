4c2eef24ca6bb0581dbf9a0eabc42beab2a456f4
BhaveshPathe <bhaveshpathe@BhaveshPatheMacAir.local>
2026-01-12 15:06:18 +0530

    chore(android): update package name to com.trunpm.truvideoreactimagesdk

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'truvideo-react-image-sdk';
+import { multiply } from '@trunpm/truvideo-react-image-sdk';
 
 export default function App() {
   const [result, setResult] = useState<number | undefined>();
